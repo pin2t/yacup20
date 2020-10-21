d5a3c033372a6568467dcc81905bdfa87a5ea823
pin2t <pin2t@mail.ru>
2020-10-21 11:11:53 +0500

    other interval types implementation

--- qualification/a.go
+++ qualification/a.go
@@ -13,31 +13,48 @@ type interval struct {
 	start, end time.Time
 }
 
-// TODO: implemet other types
-func newInterval(t string, s time.Time) interval {
+func newInterval(typ string, start time.Time) interval {
 	var end time.Time
-	var wd int = int(s.Weekday())
-	if wd == 0 { wd = 7 } // reenumerate days (1 - Moday .. 7 - Sunday)
-	switch (t) {
-	case "WEEK": end = s.AddDate(0, 0, 7 - wd)
+	switch (typ) {
+	case "WEEK": 
+		var weekday int = int(start.Weekday())
+		if weekday == 0 { weekday = 7 } // reenumerate days (1 - Monday .. 7 - Sunday)
+		end = start.AddDate(0, 0, 7 - weekday)
+	case "MONTH": 
+		year, month, _ := start.Date()
+		end = time.Date(year, month + 1, 0, 0, 0, 0, 0, time.UTC)
+	case "QUARTER": 
+		lastmonth := []int{3, 3, 3, 6, 6, 6, 9, 9, 9, 12, 12 ,12}
+		year, month, _ := start.Date()
+		end = time.Date(year, 
+			time.Month(lastmonth[int(month) - 1] + 1), 0, 0, 0, 0, 0, time.UTC)
+	case "YEAR":
+		year, _, _ := start.Date()
+		end = time.Date(year + 1, 1, 0, 0, 0, 0, 0, time.UTC)
+	case "LAST_SUNDAY_OF_YEAR":
+		year, _, _ := start.Date()
+		end = time.Date(year + 1, 1, 0, 0, 0, 0, 0, time.UTC)
+		for end.Weekday() != time.Sunday { end = end.AddDate(0, 0, -1) }
+		end = end.AddDate(0, 0, -1)
+		if end.Before(start) {
+			end = time.Date(year + 2, 1, 0, 0, 0, 0, 0, time.UTC)
+			for end.Weekday() != time.Sunday { end = end.AddDate(0, 0, -1) }
+			end = end.AddDate(0, 0, -1)
+		}
 	}
-	return interval{t, s, end}
+	return interval{typ, start, end}
 }
 
-// TODO: implemet other types
 func (i interval) next() interval {
-	var newend, newstart time.Time
-	newstart = i.end.AddDate(0, 0, 1)
-	switch (i.typ) {
-	case "WEEK": newend = i.end.AddDate(0, 0, 7)
-	}
-	return interval{i.typ, newstart, newend}
+	return newInterval(i.typ, i.end.AddDate(0, 0, 1))
 }
 
-func (i interval) include(dt time.Time) bool {
-	return (i.start.Equal(dt) || i.start.Before(dt)) && 
-		(i.end.Equal(dt) || i.end.After(dt))
+func (i interval) include(t time.Time) bool {
+	return (i.start.Equal(t) || i.start.Before(t)) && 
+		(i.end.Equal(t) || i.end.After(t))
 }
+
+const dateFormat = "2006-01-02"
 
 func main() {
 	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
@@ -48,8 +65,8 @@ func main() {
 	var err error
 	line := scanner.Text()
 	times := strings.Split(line, " ")
-	if start, err = time.Parse("2006-01-02", times[0]); err != nil { panic(err) }
-	if end, err = time.Parse("2006-01-02", times[1]); err != nil { panic(err) }
+	if start, err = time.Parse(dateFormat, times[0]); err != nil { panic(err) }
+	if end, err = time.Parse(dateFormat, times[1]); err != nil { panic(err) }
 	intervals := make([]interval, 0)
 	i := newInterval(typ, start)
 	for !i.include(end) { 
@@ -60,6 +77,6 @@ func main() {
 	intervals = append(intervals, i)
 	fmt.Println(len(intervals))
 	for _, i := range intervals { 
-		fmt.Println(i.start.Format("2006-01-02"), i.end.Format("2006-01-02"))
+		fmt.Println(i.start.Format(dateFormat), i.end.Format(dateFormat))
 	}
 }
